86be33ab3edade6aa677fe8fa8744963b428f334
Maria Carolina <mariacarolinafrancodelimaleite@gmail.com>
2025-03-27 09:05:49 -0300

    Add files via upload

---
+++ cadastro.php
@@ -0,0 +1,18 @@
+<?php
+include ("conexao.php");
+
+$nome=$_POST['nome'];
+$sobrenome=$_POST['sobrenome'];
+$email=$_POST['email'];
+$senha=$_POST['senha'];
+
+$sql="INSERT INTO cadastro(nome,sobrenome,email, senha)
+VALUES ('$nome', '$sobrenome' , '$email' '$senha')";
+
+if (mysqli_query ($conexao,$sql)) {
+   echo "usuario cadastro com sucesso";
+} else {
+    echo "erro" . mysqli_connect_erro($conexao);
+}
+mysqli_close($conexao);
+?>
---
+++ conexao.php
@@ -0,0 +1,13 @@
+<?php
+$servidor = "localhost";
+$usuario = "root";
+$senha = "";
+$dbname = "cadastro";
+
+$conexao=mysqli_connect($servidor,$usuario,$senha,$dbname);
+
+if (!$conexao){
+die("houve um erro:" . mysqli_connect_erro());
+
+}
+?>
