e066ca5f112d7af1ec23be6927b886a1afc22d47
Maria Carolina <mariacarolinafrancodelimaleite@gmail.com>
2025-04-15 09:07:04 -0300

    Delete Cadastro directory

--- Cadastro/script.js
+++
@@ -1,8 +0,0 @@
-function Visualizar(){
-    window.location.href="dashboard.php"
-}
-
-function Visualizar1(){
-    window.location.href="dashboard2.php"
-}
-
--- Cadastro/sricpt.js
+++
@@ -1,7 +0,0 @@
-function Visualizar(){
-    window.location.href="dashboard.php"
-}
-
-function Visualizar1(){
-    window.location.href="dashbord2.php"
-}
